96b1719cfffe7c7f301b39149d9be7c1196b792b
Rob Fairclough <rob@silverchip.com>
2019-08-13 17:44:50 +0100

    stub knex idea

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,16 +1,21 @@
 import * as mysql from 'mysql';
-import { dbConfig } from '../config';
+import * as Knex from 'knex';
+import { dbConfig, knexConfig } from '../config';
+const knex: Knex = require('knex')(knexConfig);
 
-const connection = mysql.createConnection(dbConfig);
+const users = knex('Users').select('*').then((users) => console.log(users)).catch(console.log);
 
-const sql = 'SELECT * FROM Sites';
+// const connection = mysql.createConnection(dbConfig);
 
-const asyncLog = (err: mysql.MysqlError, result: any) => {
-    if (err) throw err;
-    console.log({result});
-}
 
-connection.connect(async err => {
-    if (err) throw err;
-    const res = await connection.query(sql, asyncLog)
-})
+// const sql = 'SELECT * FROM Sites';
+
+// const asyncLog = (err: mysql.MysqlError, result: any) => {
+//     if (err) throw err;
+//     console.log({result});
+// }
+
+// connection.connect(async err => {
+//     if (err) throw err;
+//     const res = await connection.query(sql, asyncLog)
+// })
